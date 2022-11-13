22406c36c730463afa60937518d0ffb28831345c
dendariev <den.dariev@gmail.com>
2022-11-14 00:08:14 +0800

    8 задач

--- src/Main.java
+++ src/Main.java
@@ -19,7 +19,7 @@ public class Main {
         long b = 987678965549L;
         double c = 2.786;
         boolean d = false;
-        short e = 569;
+        char e = 569;
         short f = -159;
         int g = 27897;
         byte h = 67;
@@ -50,9 +50,47 @@ public class Main {
         int whitePaint = 2;
         int brownPaint = 4;
         int classes = totalPaints / (whitePaint + brownPaint);
-        System.out.println("В школе, где " +classes+ " классов, нужно " +whitePaint * classes+ " банок белой краски и " +brownPaint * classes+ " банок коричневой краски");
+        System.out.println("В школе, где " + classes + " классов, нужно " + whitePaint * classes + " банок белой краски и " + brownPaint * classes + " банок коричневой краски");
         // Задача 6
         System.out.println("Задача 6");
+        int banana = 80;
+        int milk = 105;
+        int icecream = 100;
+        int eggs = 70;
+        int weightGr = banana * 5 + milk * 2 + icecream * 2 + eggs * 4;
+        float weightKg = weightGr / (float) 1000;
+        System.out.println("Общий вес спорт-завтрака " + weightKg + " кг");
+        // Задача 7
+        System.out.println("Задача 7");
+        int weightToLose = 7;
+        double minWeightLoss = 0.25;
+        double maxWeightLoss = 0.5;
+        double minDays = weightToLose / minWeightLoss;
+        System.out.println("Минимальное кол-во дней " + minDays);
+        double maxDays = weightToLose / maxWeightLoss;
+        System.out.println("Максимальное кол-во дней " + maxDays);
+        double daysOnAverage = (minDays + maxDays) / 2;
+        System.out.println("Дней в среднем " + daysOnAverage);
+        // Задача 8
+        System.out.println("Задача 8");
+        int salaryMasha = 67760;
+        int salaryInYearMasha = salaryMasha * 12;
+        float newSalaryMasha = salaryMasha + (salaryMasha * (float)0.1); // float newSalaryMasha = salaryMasha + ((float)salaryMasha / 100 * 10);
+        float newSalaryInYearMasha = newSalaryMasha * 12;
+        float differenceSalaryMasha = newSalaryInYearMasha - salaryInYearMasha;
+        System.out.println("Маша теперь получает " +newSalaryMasha+ " рублей. Годовой доход вырос на " +differenceSalaryMasha+ " рублей.");
+        int salaryDenis = 83690;
+        int salaryInYearDenis = salaryDenis * 12;
+        float newSalaryDenis = salaryDenis + (salaryDenis * (float)0.1); // float newSalaryDenis = salaryDenis + ((float)salaryDenis / 100 * 10);
+        float newSalaryInYearDenis = newSalaryDenis * 12;
+        float differenceSalaryDenis = newSalaryInYearDenis - salaryInYearDenis;
+        System.out.println("Денис теперь получает " +newSalaryDenis+ " рублей. Годовой доход вырос на " +differenceSalaryDenis+ " рублей.");
+        int salaryKristina = 76230;
+        int salaryInYearKristina = salaryKristina * 12;
+        float newSalaryKristina = salaryKristina + (salaryKristina * (float)0.1); // float newSalaryKristina = salaryKristina + ((float)salaryKristina / 100 * 10);
+        float newSalaryInYearKristina = newSalaryKristina * 12;
+        float differenceSalaryKristina = newSalaryInYearKristina - salaryInYearKristina;
+        System.out.println("Кристина теперь получает " +newSalaryKristina+ " рублей. Годовой доход вырос на " +differenceSalaryKristina+ " рублей.");
 
     }
 }
