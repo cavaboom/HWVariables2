eaf06b4ff91979ecdd580377e9f0bccca402d98c
dendariev <den.dariev@gmail.com>
2022-11-13 19:32:58 +0800

    Переменные. Часть 2

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        // Задача 1
+        byte apple = 10;
+        System.out.println("Значение переменной apple с типом byte равно " + apple);
+        short carWeight = 1500;
+        System.out.println("Значение переменной carWeight с типом short равно " + carWeight);
+        int peoples = 783700000;
+        System.out.println("Значение переменной peoples с типом int равно " + peoples);
+        long money = 9223372036854775807L;
+        System.out.println("Значение переменной money с типом long равно " + money);
+        float a = 3.15F;
+        System.out.println("Значение переменной a с типом float равно " + a);
+        double b = 9.65;
+        System.out.println("Значение переменной b с типом double равно " + b);
+
+
+    }
+}
